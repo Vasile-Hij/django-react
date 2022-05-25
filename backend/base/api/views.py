95caf2e5a3cd8b653ffe1bfb103bc0dffed584c9
Vasile-Hij <vasile.hijs@gmail.com>
2022-05-25 15:06:41 +0300

    DR-3-rest-framwork https://vasilehijs.atlassian.net/browse/DR-3?atlOrigin=eyJpIjoiNjgyYThiZmQ4MzE0NGRhZWIxNTliM2E1MTViMjgyOGYiLCJwIjoiaiJ9 Testing basic /api/ routes in POSTMAN. Deleted base folder from root which were left by mistake

---
+++ backend/base/api/views.py
@@ -0,0 +1,9 @@
+from django.http import JsonResponse
+
+def getRoutes(request):
+    routes = [
+        '/api/token',
+        '/api/token/refresh'
+    ]
+
+    return JsonResponse(routes, safe=False)
