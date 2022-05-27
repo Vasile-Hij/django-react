1b6dc2a9d3b52b27ad9643cfad0e6f974d58d36b
Vasile-Hij <vasile.hijs@gmail.com>
2022-05-27 15:17:39 +0300

    DR-9-config-react https://vasilehijs.atlassian.net/browse/DR-9?atlOrigin=eyJpIjoiY2MyNTliY2M2ZjgxNGVhYWE4ZTBkZWE4MDAxNjBhMzEiLCJwIjoiaiJ9 Installed React Router DOM 5.3.0 Added PriveteRoute, Login and HomePage, AtuhCOntext and Header git commit -m DR-9-config-react

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,11 +1,25 @@
-import logo from './logo.svg';
 import './App.css';
+import { BrowserRouter as Router, Route} from "react-router-dom";
+import PrivateRoute from './utils/PrivateRoute'
+import { AuthProvider } from './context/AuthContext'
+
+import HomePage from './pages/HomePage'
+import LoginPage from './pages/LoginPage'
+import Header from './components/Header'
+
 
 function App() {
-    return ( <
-        >
-        <
-        /> );
-    }
+  return ( 
+    <div className="App">
+      <Router>
+        <AuthProvider>
+          <Header/>
+          <PrivateRoute path="/" exact component={HomePage} />
+          <Route path="/login" component={LoginPage} />
+        </AuthProvider>
+      </Router>
+    </div>      
+  );
+}
 
-    export default App;
+export default App;
