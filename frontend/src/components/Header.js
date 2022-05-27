4e25d68df1ecc8976e8bc67a47a158015a6fc1b5
Vasile-Hij <vasile.hijs@gmail.com>
2022-05-27 16:49:24 +0300

    DR-10-login-user https://vasilehijs.atlassian.net/browse/DR-10?atlOrigin=eyJpIjoiZWQ1MzBmZWJlZjAxNDg1Yjk1OTRjMDVhZGE0M2Q2ZmEiLCJwIjoiaiJ9 Used token to login user Frontend fetch now backend to retrieve access token

--- frontend/src/components/Header.js
+++ frontend/src/components/Header.js
@@ -1,12 +1,19 @@
-import React from 'react'
+import React, { useContext} from 'react'
 import { Link } from 'react-router-dom'
+import AuthContext from '../context/AuthContext'
 
 const Header = () => {
+  let {user} = useContext(AuthContext)
   return (
     <div>
         <Link to='/'>Home</Link>
         <span> | </span>
-        <Link to='/login'>Login</Link>
+        {user ? (
+          <p>Logout</p>
+        ): (
+          <Link to='/login'>Login</Link>
+        )}
+        {user && <p> Hello {user.username}</p>}
         
     </div>
   )
--- frontend/src/context/AuthContext.js
+++ frontend/src/context/AuthContext.js
@@ -1,13 +1,40 @@
-import { createContext } from 'react'
+import { createContext, useState } from 'react'
+import jwt_decode from 'jwt-decode'
 
 const AuthContext = createContext()
 
 export default AuthContext;
 
-
 export const AuthProvider = ({children}) => {
+    
+    let [authTokens, setAuthTokens] = useState(null)
+    let [user, setUser] = useState(null)
+    
+    let loginUser = async (e )=> {
+        e.preventDefault()
+        let response = await fetch('http://127.0.0.1:8000/api/token/', {
+            method:'POST',
+            headers:{
+                'Content-Type':'application/json'
+            },
+            body:JSON.stringify({'username':e.target.username.value, 'password':e.target.password.value})
+        })
+        let data = await response.json()
+        if (response.status === 2000) {
+            setAuthTokens(data)
+            setUser(jwt_decode(data.access))
+        }else{
+            alert('Something went wrong!')
+        }
+        
+    }
+    
+    let contextData = {
+        user:user, 
+        loginUser:loginUser
+    }
     return (
-        <AuthContext.Provider value={{'name':'Tester'}} >
+        <AuthContext.Provider value={{contextData}} >
             {children}
         </AuthContext.Provider>
     )
