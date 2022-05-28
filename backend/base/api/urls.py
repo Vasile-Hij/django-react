765decdbe20013411569ccb31b7e81757326c632
Vasile-Hij <vasile.hijs@gmail.com>
2022-05-28 21:33:28 +0300

    DR-13-setup-user-in-db https://vasilehijs.atlassian.net/browse/DR-13?atlOrigin=eyJpIjoiNThiYTYwMjY5NjgyNDZhMWI2Mzc3YjQwZTNjYzAyMTkiLCJwIjoiaiJ9 Added models and serializers for Notes Added permission for user authenticated only

--- backend/base/api/urls.py
+++ backend/base/api/urls.py
@@ -7,6 +7,8 @@ from rest_framework_simplejwt.views import (
 
 urlpatterns = [
     path('', views.getRoutes),
+    path('notes/', views.getNotes),
+    
     path('token/', MyTokenObtainPairView.as_view(), name='token_obtain_pair'),
     path('token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
 ]
