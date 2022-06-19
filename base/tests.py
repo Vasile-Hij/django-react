f9fcbb2cd208a4386f90d6de947c5ace59b8f54f
Vasile-Hij <vasile.hijs@gmail.com>
2022-06-19 23:59:20 +0300

    DR-28-create-search-and-request https://vasilehijs.atlassian.net/browse/DR-28?atlOrigin=eyJpIjoiYmQyZGFkMWM5YmViNDU1MTljMDZlNDdmM2NmMTU3MjEiLCJwIjoiaiJ9 Added class for Open Weather Map Removed Google API due new Open weather API solve location query

--- base/tests.py
+++ base/tests.py
@@ -1,3 +1,19 @@
 from django.test import TestCase
+from base.views.weather_api import LocationApi as locationApi
 
 # Create your tests here.
+url = ''
+unit_url = 'c'
+location = 'Bucharest'
+country_code = ''
+
+
+key_and_unit = locationApi()
+test_one = key_and_unit._add_unit_and_api_key(url, unit_url)
+print(f'Testing "add_unit_and_api_key" {test_one}')
+
+partial_location = locationApi()
+test_two = partial_location.current_location_weather(location, unit_url )
+print(f'Testing "current_location_weather" {test_two }')
+
+
---
+++ base/urls/weather_urls.py
@@ -0,0 +1,5 @@
+from django.urls import path
+from base.views import weather_views as views
+urlpatterns = [
+    # path('', views.createLocation, name='createLocation'),
+]   
